3b586b7da415e7c3488bc8a6eb4eabfc0ac0785c
gianfrancococchieri-commits <gianfranco.cocchieri@gmail.com>
2026-04-09 08:49:38 +0200

    Create app.js

---
+++ app.js
@@ -0,0 +1,14 @@
+{
+  "name": "mfa-demo",
+  "version": "1.0.0",
+  "main": "app.js",
+  "scripts": {
+    "start": "node app.js"
+  },
+  "dependencies": {
+    "express": "^4.19.2",
+    "body-parser": "^1.20.2",
+    "express-session": "^1.17.3",
+    "otplib": "^12.0.1"
+  }
+}
